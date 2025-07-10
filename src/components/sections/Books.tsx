b8743e6e87c2f70a047377b1c7f4fc170823c068
tienloc <tienloc1401@gmail.com>
2025-07-10 19:17:40 +0700

    ui: update Book section

---
+++ src/components/sections/Books.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import { ChevronRight, ChevronLeft } from 'lucide-react';
+import { BasicNews } from '@/interfaces/front-news';
+import { ThumbnailCard } from '../elements/NewsCard';
+import Headline from '@/components/elements/Headline';
+
+export default function Books({ data }: { data: BasicNews[] }) {
+  return (
+    <section>
+      <Headline label="BOOKS" />
+      <div className="relative mt-4">
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={24}
+          slidesPerView={2}
+          loop={true}
+          navigation={{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }}
+          breakpoints={{
+            1024: { slidesPerView: 5 },
+            768: { slidesPerView: 3 },
+          }}>
+          {data?.map((news) => (
+            <SwiperSlide key={news.id}>
+              <ThumbnailCard
+                altText={news.altText}
+                href={news.href}
+                imageUrl={news.imageUrl}
+                title={news.title}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <button
+          className="absolute top-1/2 -translate-y-1/2 -left-5 z-10 bg-white p-2 rounded-full border border-gray-200 hover:bg-gray-100 swiper-button-prev"
+          aria-label="Previous Slide">
+          <ChevronLeft />
+        </button>
+        <button
+          className="absolute top-1/2 -translate-y-1/2 -right-5 z-10 bg-white p-2 rounded-full border border-gray-200 hover:bg-gray-100 swiper-button-next"
+          aria-label="Next Slide">
+          <ChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
